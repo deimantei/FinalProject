3a121fbdb70db645d39f0999bdb473deacccfa7e
unknown <deimantestankunaitemail@gmail.com>
2022-09-16 23:43:26 +0300

    Changes 3 by Deimante

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,27 +8,52 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
-app.get('/get-allCustomer', (req,res) =>{
-    fs.readFile('AllCustomers.json', 'utf-8', (err, data) => {
-        res.send(JSON.stringify({'status':200, 'error':null, 'response': JSON.parse(data)}))
-    })
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'',
+    database:'theband'
 })
-app.post('/add-customer', (req,res) => {
-const newCustomer = {
-    firstname: req.body.name,
-    lastname: req.body.lastname,
-    phone: req.body.phone,
-    email: req.body.email
-}
-fs.readFile('AllCustomers.json', 'utf-8', (err, data) => {
-    const customers = JSON.parse(data);
-    customers.customers.push(newCustomer);
-    fs.writeFile('AllCustomers.json', JSON.stringify(customers, null, 3), function(err){
-        res.send('Customer is added');
-        });
+
+connection.connect((error) => {
+    if(error)
+        throw error;
+    console.log('Connected to the database');
+})
+
+app.get('/get-allCustomer', (req,res) => {
+    const sqlQuerry = 'SELECT * from customers';
+    connection.query(sqlQuerry,(error, results) => {
+        if(error)
+            throw error;
+        res.send(JSON.stringify({'status':200, 'error':null, 'response': {customers: results}}))
     })
 })
 
+app.post('/add-customer', (req,res) => {
+    const newCustomer = {
+        firstname: req.body.name,
+        lastname: req.body.lastname,
+        bdate: req.body.bdate,
+        phone: req.body.phone,
+        email: req.body.email,
+    };
+    
+    const sqlQuery = "INSERT INTO customers (firstname,lastname,bdate, email,phone) VALUES (?,?,?,?,?)";
+    connection.query(sqlQuery, [newCustomer.firstname, newCustomer.lastname, newCustomer.bdate,
+        newCustomer.email, newCustomer.phone,
+    ], (error, results) => {
+        if (error)
+            throw error;
+        res.send(JSON.stringify({
+            "status": 200,
+            "error": null,
+            "response": "Customer ID : " + results.insertId + " created!"
+        }))
+    })
+})
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 })
